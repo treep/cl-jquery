4bcae7d78184995f77000a222a4033a441cb0dd5
treep <zena.treep@gmail.com>
2010-06-16 08:23:05 +0400

    Added new plugins - jquery.form, jquery.json and jquery.ui.

--- cl-jquery.asd
+++ cl-jquery.asd
@@ -3,8 +3,12 @@
 ;;;
 
 (asdf:defsystem cl-jquery
+  :description "JQuery framework and plugins distribution for CL."
   :version "1.4.2"
   :serial  t
-  :components ((:static-file "jquery.min.js")
+  :components ((:static-file "jquery.js")
+               (:static-file "jquery.form.js")
+               (:static-file "jquery.json.js")
+               (:static-file "jquery.ui.js")
                (:static-file "jquery.console.js")
                (:file "cl-jquery")))
--- cl-jquery.lisp
+++ cl-jquery.lisp
@@ -4,11 +4,18 @@
 
 (defpackage #:cl-jquery
   (:use #:cl #:asdf)
-  (:export #:*jquery.min.js*
+  (:export #:*jquery.js*
+           #:*jquery.form.js*
+           #:*jquery.json.js*
+           #:*jquery.ui.js*
            #:*jquery.console.js*))
 
 (in-package #:cl-jquery)
 
 (defvar *cl-jquery-path*    (component-pathname (find-system '#:cl-jquery)))
-(defvar *jquery.min.js*     (merge-pathnames "jquery.min.js" *cl-jquery-path*))
+
+(defvar *jquery.js*         (merge-pathnames "jquery.js"         *cl-jquery-path*))
+(defvar *jquery.form.js*    (merge-pathnames "jquery.form.js"    *cl-jquery-path*))
+(defvar *jquery.json.js*    (merge-pathnames "jquery.json.js"    *cl-jquery-path*))
+(defvar *jquery.ui.js*      (merge-pathnames "jquery.ui.js"      *cl-jquery-path*))
 (defvar *jquery.console.js* (merge-pathnames "jquery.console.js" *cl-jquery-path*))
